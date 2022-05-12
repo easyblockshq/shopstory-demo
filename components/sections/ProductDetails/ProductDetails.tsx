2fe90efc844c6a5f04ccfd09124324552b380506
“Karol <karolkro@wp.pl>
2022-05-12 09:13:25 +0200

    PDP gallery - contain mode

--- components/sections/ProductDetails/ProductDetails.tsx
+++ components/sections/ProductDetails/ProductDetails.tsx
@@ -1,5 +1,5 @@
 import styles from './productDetails.module.css'
-import React, { FC, useState } from 'react'
+import React, { FC, useEffect, useState } from 'react'
 import { ShopifyProduct } from '../../../types'
 import { formatPrice } from '../../../utils/formatPrice'
 import AddToBagButton from '../../common/AddToBagButton/AddToBagButton'
@@ -12,6 +12,8 @@ import { Toast } from '../../common/Toast/Toast'
 const ProductDetails: FC<{ product: ShopifyProduct }> = ({ product }) => {
   const [isToastActive, setIsToastActive] = useState(false)
   const [isToastVisible, setIsToastVisible] = useState(false)
+  const [layout, setLayout] = useState<'fill' | 'responsive'>('fill')
+  const [objectFit, setObjectFit] = useState<'cover' | 'contain' | undefined>('contain')
 
   const openToast = () => {
     if (!isToastActive && !isToastVisible) {
@@ -26,22 +28,37 @@ const ProductDetails: FC<{ product: ShopifyProduct }> = ({ product }) => {
     }
   }
 
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth < 768) {
+        setLayout('fill')
+        setObjectFit('contain')
+      } else {
+        setLayout('responsive')
+        setObjectFit(undefined)
+      }
+    }
+
+    window.addEventListener('resize', handleResize)
+
+    return () => {
+      window.removeEventListener('resize', handleResize)
+    }
+  })
+
   return (
     <div className={styles.wrapper}>
       <div className={styles.gallery}>
         {product.media &&
           product.media.map((item, i) => {
-            const aspectRatio = item.mediaObject.width / item.mediaObject.height
-            const minWidth = aspectRatio ? aspectRatio * 520 + 'px' : undefined
             return (
-              <div
-                key={i}
-                className={styles.galleryItem}
-                style={{
-                  minWidth: minWidth
-                }}
-              >
-                <Media media={item} sizes={`(min-width: 1000px) calc(100vw - 480px), ${minWidth}`} />
+              <div key={i} className={styles.galleryItem}>
+                <Media
+                  media={item}
+                  sizes={`(min-width: 1000px) calc(100vw - 480px), 100vw`}
+                  layout={layout}
+                  objectFit={objectFit}
+                />
               </div>
             )
           })}
