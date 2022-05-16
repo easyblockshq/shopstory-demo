216274f07d2806b44323ef58b97d5ea55bd7162b
“Karol <karolkro@wp.pl>
2022-05-16 23:54:14 +0200

    Asana tasks

--- components/common/ProductCard/ProductCard.tsx
+++ components/common/ProductCard/ProductCard.tsx
@@ -8,7 +8,12 @@ import styles from './productCard.module.css'
 
 const ProductCard: FC<{ product: ShopifyProduct }> = ({ product }) => {
   return (
-    <div className={styles.wrapper}>
+    <div
+      className={[
+        styles.wrapper,
+        product.relatedProducts && product.relatedProducts?.length > 1 ? styles.wrapperWithVariants : ''
+      ].join(' ')}
+    >
       <Link href={'/products/' + product.handle}>
         <a>
           <div className={styles.thumbnail}>
@@ -23,34 +28,35 @@ const ProductCard: FC<{ product: ShopifyProduct }> = ({ product }) => {
             )}
           </div>
           <div className={styles.info}>
+            {product.relatedProducts && product.relatedProducts?.length > 1 && (
+              <div className={styles.related}>
+                {product.relatedProducts.map((relatedProduct, i) => {
+                  let relatedLinkClasses = [styles.relatedProduct]
+                  if (product.handle === relatedProduct.handle) {
+                    relatedLinkClasses.push(styles.relatedProductActive)
+                  }
+
+                  if (relatedProduct.color && relatedProduct.color?.length > 0) {
+                    return (
+                      <Link href={'/products/' + relatedProduct.handle} key={i}>
+                        <a className={relatedLinkClasses.join(' ')}>
+                          {relatedProduct.primaryImage ? (
+                            <Media media={relatedProduct.primaryImage} sizes="80px" />
+                          ) : (
+                            <div className={styles.placeholder}>No image</div>
+                          )}
+                        </a>
+                      </Link>
+                    )
+                  }
+                })}
+              </div>
+            )}
             <h2 className={styles.title}>{product.title}</h2>
             {product.price && <p className={styles.priceStandard}>{formatPrice(product.price)}</p>}
           </div>
         </a>
       </Link>
-      {product.relatedProducts && product.relatedProducts?.length > 1 && (
-        <div className={styles.related}>
-          {product.relatedProducts.map((relatedProduct, i) => {
-            let relatedLinkClasses = [styles.relatedProduct]
-            if (product.handle === relatedProduct.handle) {
-              relatedLinkClasses.push(styles.relatedProductActive)
-            }
-
-            if (relatedProduct.color && relatedProduct.color?.length > 0) {
-              return (
-                <Link href={'/products/' + relatedProduct.handle} key={i}>
-                  <a className={relatedLinkClasses.join(' ')}>
-                    <div
-                      className={styles.relatedColor}
-                      style={{ backgroundColor: getCollectionColor(relatedProduct.color[0].name)?.hex }}
-                    ></div>
-                  </a>
-                </Link>
-              )
-            }
-          })}
-        </div>
-      )}
     </div>
   )
 }
--- components/common/ToggleColorButton/ToggleColorButton.tsx
+++ components/common/ToggleColorButton/ToggleColorButton.tsx
@@ -1,5 +1,6 @@
 import styles from './ToggleColorButton.module.css'
 import React, { FC, ReactNode } from 'react'
+import Tick from '../../icons/Tick'
 
 export const ToggleColorButton: FC<
   React.HTMLProps<HTMLButtonElement> & { type?: 'submit' | 'reset' | 'button'; color: string }
@@ -12,7 +13,9 @@ export const ToggleColorButton: FC<
 
   return (
     <button className={classes.join(' ')} aria-selected={selected} {...restProps}>
-      <span className={styles.colorInfo} style={{ backgroundColor: color }}></span>
+      <span className={styles.colorInfo} style={{ backgroundColor: color }}>
+        <Tick />
+      </span>
       {children}
     </button>
   )
